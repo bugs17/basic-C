582d6c59d7564a95d14b35eaa4c2d3a926fa0f62
Bugs PC <rnaldi491@gmail.com>
2023-11-06 22:42:37 +0900

    data type long

--- type_data/type_data.c
+++ type_data/type_data.c
@@ -36,13 +36,67 @@ int main(){
   // kalau ko tau ko trabutuh nilai negatif nanti tinggal ko tambah unsigned di depan type data saja jadi minimum-maximum numbernya jadi 0 sampe +255
   char seratus = 100;
 
+  // menggunakan unsigned
+  // jadinya nilai minimum adalah 0
+  // nilai minusnya di pindahkan ke depan jadi sekarang bisa menyimpan nilai hingga +255
+  // menggunakan 1 byte tetap. dan cara aksesnya juga sama bisa %d dan %c untuk asci character
+  unsigned char duaratus = 254; 
 
-  printf("%f\n",c);
-  printf("%0.15f\n",nilai_float); // tong harus tambahkan 0.15 di depan special key untuk menampilkan berapa digit setelah koma disini tong ingin tampilkan 15 digit jadi 0.15
-  printf("%d\n",is_Sold); // %d karna hasilnya antara 0 dan 1 
-  printf("%d\n",is_Ready); // ini juga sama saja
-  printf("%c\n",seratus); // akses char sebagai char jadi de akan hasilkan nilai asci yaitu d kecil
-  printf("%d\n",seratus); // ini akan mengakses nilainya sesuai dengan nilai yang di masukan yaitu integer
+
+  // gunakan short pada integer ini menggunakan 2 byte memory
+  // dan bisa menampung (-32,768 sampe +32,767)
+  // kalau tidak mau gunakan angka minus bisa pake unsigned jadi nilai positif yang di simpan bisa lebih besar
+  // jadi kalo berikan nilai lebih dari positif 32767 maka de akan kembali ke nilai paling awal yaitu -32768 
+  short int contoh_short = 32767;
+
+
+  // kali ini coba pake unsigned agar dapat menyimpan nilai positif integer lebih besar
+  // karena minusnya di hilangkan jadi tetap menggunakan 2 byte memory
+  // hanya saja nilai maximum hingga 65,535 kalo lebih dari ini maka de akan memulai dari awal lagi 0 ke atas tergantung lebihnya
+  unsigned short int tes_integer_unsigned = 65535;
+
+
+  // int menyimpan nilai dari (-2,147,483,648 sampe +2,147,483,647)
+  // dan de menggunakan 4 byte memory makanya bisa simpan nilai sampe besar begitu
+  // dan kalau mau nilai psotof lebih besar dan tidak butuh menyimpan nilai negatif maka tinggal tambah unsigned saja
+  int j = 2147483647;
+
+  // ini juga sama menggunakan 4 byte memory tetapi tidak menggunakan nilai negatif
+  // jadi nilai positif jadi lebih besar hingga (4,249,967,295) dan minumum 0
+  // cara pemanggilannya berbeda yaitu menggunakan %u
+  unsigned int x = 4147483647;
+
+
+  // dan kalo ko mau simpan data integer yang besar skali ko bisa pake long long int
+  // kenappa long nya dua? karena int biasa di atas sebenarnya sudah pake long secara default jadi tong cukup tulis sebagai int saja
+  //  tapi kalo ko mau pake long long integer ini nanti bisa simpan nilai sampe (-9 quantiliun sampe +9 quantiliun)
+  // simpan nilai besar skali dan de pake memory 8 byte
+  // cara pemanggilannya %lld
+  long long int f = 1273979874238934234; 
+
+  // kalau mau lebih besar lagi bisa pake unsigned tetapi ingat bahwa tidak ada nilai negatif
+  // dan cara penggunaannya %llu
+  // 8-byte memory juga
+  // dan tong akan dapat warning bahwa nilai terlalu besar untuk ignore warnung ini tong bisa tambah U di belakang nilainya jadi kaya begini 
+  // 14273979874238934234U
+  unsigned long long int k = 14273979874238934234U;
+
+  printf("%f\n", c);
+  printf("%0.15f\n", nilai_float); // tong harus tambahkan 0.15 di depan special key untuk menampilkan berapa digit setelah koma disini tong ingin tampilkan 15 digit jadi 0.15
+  printf("%d\n", is_Sold); // %d karna hasilnya antara 0 dan 1 
+  printf("%d\n", is_Ready); // ini juga sama saja
+  printf("%c\n", seratus); // akses char sebagai char jadi de akan hasilkan nilai asci yaitu d kecil
+  printf("%d\n", seratus); // ini akan mengakses nilainya sesuai dengan nilai yang di masukan yaitu integer
+  printf("%d\n", duaratus); // char
+  printf("%c\n", duaratus); // unsigned char
+  printf("%d\n", contoh_short); // short int
+  printf("%d\n", tes_integer_unsigned); // unsigned short int
+  printf("%d\n", j); // int
+  printf("%u\n", x); // unsigned int %u
+  printf("%lld\n", f); // long long int %lld
+  printf("%llu\n", k); // unsigned long long int %lld
+
+
 
   return 0;
 }
