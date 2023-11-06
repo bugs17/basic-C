ccc1d7fb542840f5003c6d572370e5a1f5a0fe7a
Bugs PC <rnaldi491@gmail.com>
2023-11-06 20:02:25 +0900

    basic C

---
+++ Hallo.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+
+
+  /**
+   * ini contoh penggunaan enter menggunakan karakter '\n'
+   * dan jika ingin memberikan tab maka gunakan '\t'
+   * 
+  */
+  printf("hallooo bugs\n");
+  printf("ini bari baru\n");
+  return 0;
+}
